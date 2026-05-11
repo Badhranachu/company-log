89b09fb4a1cbe5cce090c2b3ec1f6123bc730275
badhranachu <www.badhranks678@gmail.com>
2026-05-11 17:01:49 +0530

    fix django static root

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -92,6 +92,8 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
+
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
